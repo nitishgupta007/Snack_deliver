b945f1d828950a7bfeb3031dd84f83baf90324d6
nitishgupta7 <nitish@sendinblue.com>
2019-01-14 14:37:29 +0530

    Full structure & add add ingredients dynamically

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 2,
+            cheese: 2,
+            bacon: 1
+        }
+    }
+    render() {
+        return(
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Tools</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
