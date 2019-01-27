5040760263ac062205f78b5f1a236728f547d4d7
nitishgupta7 <nitish@sendinblue.com>
2019-01-27 09:32:49 +0530

    routing & addition & deletion

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENTS_PRICES = {
+    salad: .3,
+    meat: .5,
+    cheese: 1.3,
+    bacon: .7
+};
 
 class BurgerBuilder extends Component {
     state = {
@@ -10,13 +17,47 @@ class BurgerBuilder extends Component {
             meat: 0,
             cheese: 0,
             bacon: 0
-        }
+        },
+        totalPrice: 4
     }
+
+addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1 ;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENTS_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+}
+
+removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0)
+    {
+        return;
+    }
+    const updatedCount = oldCount - 1 ;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENTS_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+}
     render() {
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls 
+                ingredientsAdded={this.addIngredientHandler}
+                ingredientsRemove={this.removeIngredientHandler}
+                />
             </Aux>
         );
     }
---
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class PizzaBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>Pizza Builder</div>
+            </Aux>
+        );
+    }
+}
+
+export default PizzaBuilder;
